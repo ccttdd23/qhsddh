99aa8c56d427926309935bdd37d3e5ec259f80b9
CKTTDD <126555864+ccttdd23@users.noreply.github.com>
2023-04-10 18:55:42 +0700

    init commit

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1t0pbQbUSsywupP-c77VxHzDt2Kq0bW1E8xQIpVRqmYY/';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
